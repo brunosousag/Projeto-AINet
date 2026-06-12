694528d8ec47c15bf443371f3922ff68f8f61b5d
JDRobalo <joao.diogo.robalo@gmail.com>
2026-06-12 20:55:44 +0100

    Delete ai-laravel-6-full directory

--- ai-laravel-6-full/resources/views/components/categories/filter-card.blade.php
+++
@@ -1,5 +0,0 @@
-<form method="GET" action="{{ $filterAction }}" {{ $attributes->class(['flex flex-wrap items-end gap-3']) }}>
-    <flux:input name="search" label="Search" value="{{ $search }}" />
-    <flux:button type="submit" icon="magnifying-glass" variant="primary">Filter</flux:button>
-    <flux:button :href="$resetUrl" icon="x-mark" variant="ghost">Reset</flux:button>
-</form>
--- ai-laravel-6-full/resources/views/tshirt-images/create.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layouts::main-content title="New Catalog Image"
-                         heading="Create catalog image"
-                         subheading="Store a new public t-shirt image">
-    <form method="POST" action="{{ route('tshirt-images.store') }}" enctype="multipart/form-data" class="space-y-4">
-        @csrf
-        @include('tshirt-images.partials.fields', ['mode' => 'create'])
-        @include('partials.form-buttons', ['entity' => 'catalog-image', 'save' => true, 'cancel' => true])
-    </form>
-</x-layouts::main-content>
